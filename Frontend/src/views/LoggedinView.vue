7d91d773042d543d4bb649cfc6cb79c0ece689e8
Miguel <miguelitorruiz@gmail.com>
2023-05-26 14:14:34 +0200

    no mas test, UI bonita, campos cambiar

--- Frontend/src/views/LoggedinView.vue
+++ Frontend/src/views/LoggedinView.vue
@@ -6,53 +6,64 @@ import { api } from '@/middleware'
 
 
 const store = userStore()
-console.log(store)
 
 var react = reactive({
-  data: '',
+  data: {},
   imageUp: null,
   image: null
 })
+
+var re=reactive({})
 
 onBeforeMount(async () => {
 
   console.log("BEFORE MOUNT LOGGEDIN")
 
   returnUserData()
-  console.log(react.data,'123321')
 })
 
 onMounted(async () => {
   
-  console.log('react', react.data, 'holi')
-  
   getImg()
-
+console.log('re',re);
 })
 
 async function returnUserData(){
-
-react.data = await store.get()
-console.log(react.data,'porfin')
+Object.keys(store.data).forEach(key => {
+  let p = key.charAt(0).toUpperCase() + key.slice(1)
+  eval(`re.${p} = store.data[key]`)
+  console.log(re)
+  if(re.Id) delete re.Id
+});
+console.log(re,'porfin')
 }
 
-function test() {
+async function test() {
   const formData = new FormData()
   formData.append('image', react.imageUp)
   formData.append('id', store.data.id)
   console.log('Imagen subida: ', react.imageUp)
   api
     .post('uploadImg', formData)
-    .then(() => {
-      getImg()
+    .then((res) => {
+      console.log(res)
     })
     .catch((err) => {
       console.log('algo fallo', err)
     })
+
 }
 
 function onFileChange(event) {
+  if(event.target.files[0]===undefined) return
   react.imageUp = event.target.files[0]
+  console.log(react.imageUp);
+  const fr = new FileReader()
+  fr.onload = ()=>{
+    const srcData = fr.result
+    re.Img = srcData
+  }
+  fr.readAsDataURL(react.imageUp)
 }
 
 async function getImg() {
@@ -60,7 +71,8 @@ async function getImg() {
     api
       .get('getImg', { params: { id: store.data.id } })
       .then((res) => {
-return res
+        console.log(res);
+        re.Img = res.data.img
        // react.image = `data:${res.data.ext};base64,${res.data.img}`
 
       })
@@ -72,41 +84,59 @@ return res
 
 const fileInput=ref(null)
 const isHovering=ref(false)
+const isEditing=ref(false)
 function selectFile(){
-  fileInput.value.click()
+  if(isEditing.value===true)fileInput.value[0].click()
 }
+
 </script>
 
 <template>
+  <div style="height: 50px;"></div>
+       
 
-  <v-row class="mx-3 py-1">
 
-
-  </v-row>
-  
-  <template v-for="(value, key) in react.data">
-  
-    
-    <template v-if="key==='img'">
-      <!--eslint-disable-next-line vue/valid-v-for-->
-    <v-row class="mx-3 py-1" style="display: flex; align-items: center;">
-    imagen:
-    <img :src="react.data.img" width="100" height="100" class="mx-4" @click="selectFile" 
-    :style="{'cursor': isHovering ? 'pointer' : 'default'}"
+  <v-col class="mx-3 py-1" style="display: flex; align-items: center; top:10px;z-index: 20;">
+    <v-avatar size="125" color="rgba(0,0,0,0.5)" rounded="50%" style="border: 1px grey solid;" elevation="16" class="avatar">
+    <v-img :src='re.Img' width="125" height="125" class="mx-4" @click="selectFile" 
+    :style="{'cursor': isHovering && isEditing ? 'pointer' : 'default', 
+    'opacity': isHovering && isEditing ? '0.5' : '1.0',
+  }"
     @mouseenter="isHovering = true" @mouseleave="isHovering = false"/>
-    <form @submit.prevent="test">
+    </v-avatar>
+    <template v-if="isEditing===true">
+      <form @submit.prevent="test">
     <v-btn type="submit" :style="{'display': react.imageUp ? 'initial' : 'none'}">Upload</v-btn>
     <input type="file" @change="onFileChange" ref="fileInput" style="display:none;"/>
     </form>
-  </v-row>
+
+    </template>
+  </v-col>
+  <v-sheet class="my-n16 pt-4">
+  <div>
+  <v-card  class="ma-2 py-3 pt-16" elevation="0">
+  <template v-for="(value, key) in re">
+  
+    
+    <template v-if="key==='Img'">
+      <!--eslint-disable-next-line vue/valid-v-for-->
+
   </template>
 <!--eslint-disable-next-line vue/valid-v-for-->
-    <v-row class="mx-3 py-1" v-if="key !== 'img'">{{ key }}: {{ value }}</v-row>
+    <v-card-item class=" ml-1 py-0" v-if="key !== 'Img'" >
+    <v-text-field :label="key" :value="value" active="true"></v-text-field></v-card-item>
   </template>
+  <v-card-actions>
+  <v-btn @click="isEditing = !isEditing" variant="outlined">Editar</v-btn>
+  </v-card-actions>
+  </v-card>
+  </div>
+  </v-sheet>
 </template>
 
 <style>
 :root {
   --toastify-color-progress-colored: rgb(29, 179, 29);
 }
+
 </style>
